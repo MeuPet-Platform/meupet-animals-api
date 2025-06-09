b15d49f759423fc1256f5c24ff616233026d6e3e
Sooties <soousadiegoo@gmail.com>
2025-06-09 20:37:56 -0300

    feat(entity): Cria entidade vacina

---
+++ src/main/java/com/meupet/api/entity/VacinaEntity.java
@@ -0,0 +1,27 @@
+package com.meupet.api.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "vacina")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class VacinaEntity extends PanacheEntity {
+
+    public String tipoVacina;
+    public LocalDate dataVacina;
+    public LocalDate revacina;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_animal")
+    public AnimalEntity animal;
+}
